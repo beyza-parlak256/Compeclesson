27d144f9f59128d798dad2b72877a31e78f23508
Beyza Parlak <beyza.parlak98@erzurum.edu.tr>
2021-04-06 23:21:26 +0300

    +added: new rotate script +refactor: basic lesson: added vector

---
+++ Assets/MyCode/LessonsBasic/FirstLessonScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public int tamSayi = 1;
+        [SerializeField] int tamSayi2 = 100;
+        float ondalikSayi = 0.25f;
+        float ondalikSayi2 = 5.2342f;
+        bool dogru = true;
+        bool yanlis2 = false;
+        [SerializeField] private Vector3 ucFloat = new Vector3(1, 2, 3);
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
